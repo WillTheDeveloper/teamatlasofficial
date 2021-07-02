9c1810dbac601784d34ec30e4378b61ce8d07876
Will <will@teamatlas.cloud>
2021-07-02 16:13:07 +0100

    Started working on home page

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            teal: colors.teal,
+            cyan: colors.cyan,
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/forms'), require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'),
+  ],
+}
