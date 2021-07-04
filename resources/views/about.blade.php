9e7f2958fd4f60c1ecc849bab35864151c51c55d
Will <will@teamatlas.cloud>
2021-07-04 16:56:23 +0100

    Updated hover on all pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,7 @@
 <body>
 <div class="bg-white">
     <div class="relative overflow-hidden">
-        <header class="relative">
+        <header x-data="{ mobileDropdown: false }" class="relative">
             <div class="bg-gray-900 pt-6">
                 <nav class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
                      aria-label="Global">
@@ -26,13 +26,13 @@
                                 <i class="fas fa-3x fa-cloud text-white"></i>
                             </a>
                             <div class="-mr-2 flex items-center md:hidden">
-                                <button type="button"
-                                        class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
+                                <button @click="mobileDropdown = true" type="button"
+                                        class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800"
                                         aria-expanded="false">
                                     <span class="sr-only">Open main menu</span>
                                     <!-- Heroicon name: outline/menu -->
                                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                         viewBox="0 0 24 24" stroke="white" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                               d="M4 6h16M4 12h16M4 18h16"/>
                                     </svg>
@@ -60,19 +60,24 @@
                 From: "opacity-100 scale-100"
                 To: "opacity-0 scale-95"
             -->
-            <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-                <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div x-show="mobileDropdown"
+                 x-transition:enter="transition ease-out duration-200"
+                 x-transition:enter-start="opacity-0 translate-y-1"
+                 x-transition:enter-end="opacity-100 translate-y-0"
+                 x-transition:leave="transition ease-in duration-150"
+                 x-transition:leave-start="opacity-100 translate-y-0"
+                 x-transition:leave-end="opacity-0 translate-y-1" @click.away="mobileDropdown = true" class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
+                <div class="rounded-lg shadow-md bg-gray-600 ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div class="px-5 pt-4 flex items-center justify-between">
                         <div>
-                            <img class="h-8 w-auto"
-                                 src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg" alt="">
+                            <i class="fas fa-3x fa-cloud text-white"></i>
                         </div>
                         <div class="-mr-2">
-                            <button type="button"
-                                    class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                            <button @click="mobileDropdown = false" type="button"
+                                    class="bg-gray-600 rounded-md p-2 inline-flex items-center justify-center text-gray-400">
                                 <span class="sr-only">Close menu</span>
                                 <!-- Heroicon name: outline/x -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
                                      viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                           d="M6 18L18 6M6 6l12 12"/>
@@ -82,26 +87,14 @@
                     </div>
                     <div class="pt-5 pb-6">
                         <div class="px-2 space-y-1">
-                            <a href="#"
-                               class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Product</a>
+                            <a href="/"
+                               class="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-gray-50 focus:bg-gray-500">Home</a>
 
-                            <a href="#"
-                               class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Features</a>
+                            <a href="/features"
+                               class="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-gray-50 focus:bg-gray-500">Features</a>
 
-                            <a href="#"
-                               class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Marketplace</a>
-
-                            <a href="#"
-                               class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Company</a>
-                        </div>
-                        <div class="mt-6 px-5">
-                            <a href="#"
-                               class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700">Start
-                                free trial</a>
-                        </div>
-                        <div class="mt-6 px-5">
-                            <p class="text-center text-base font-medium text-gray-500">Existing customer? <a
-                                    href="#" class="text-gray-900 hover:underline">Login</a></p>
+                            <a href="/about"
+                               class="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-gray-50 focus:bg-gray-500">About Us</a>
                         </div>
                     </div>
                 </div>
