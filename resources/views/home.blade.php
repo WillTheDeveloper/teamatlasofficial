95e06d5662b4e1c65283d92e351e65b4fefcbdcc
Will <will@teamatlas.cloud>
2021-07-04 16:44:11 +0100

    Sorted colours for nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,14 +63,14 @@
                     To: "opacity-0 scale-95"
                 -->
                 <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-                    <div class="rounded-lg shadow-md bg-gray-900 ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div class="rounded-lg shadow-md bg-gray-600 ring-1 ring-black ring-opacity-5 overflow-hidden">
                         <div class="px-5 pt-4 flex items-center justify-between">
                             <div>
                                 <i class="fas fa-3x fa-cloud text-white"></i>
                             </div>
                             <div class="-mr-2">
                                 <button type="button"
-                                        class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400">
+                                        class="bg-gray-600 rounded-md p-2 inline-flex items-center justify-center text-gray-400">
                                     <span class="sr-only">Close menu</span>
                                     <!-- Heroicon name: outline/x -->
                                     <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
