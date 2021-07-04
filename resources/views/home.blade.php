11ebdafbb99072202ee98cd6dc48f363ff93cc67
Will <will@teamatlas.cloud>
2021-07-04 15:59:06 +0100

    Removed colour change on focus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
                             </div>
                             <div class="-mr-2">
                                 <button type="button"
-                                        class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                                        class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400">
                                     <span class="sr-only">Close menu</span>
                                     <!-- Heroicon name: outline/x -->
                                     <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
